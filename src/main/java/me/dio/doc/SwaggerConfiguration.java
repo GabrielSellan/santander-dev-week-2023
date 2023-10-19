6d7e9dbcd4515f171e5c8169054c2a9ed2ca5939
GabrielSellan <gabrielsellan2002@gmail.com>
2023-10-18 22:24:03 -0300

    Bugfix CORS(3)

---
+++ src/main/java/me/dio/doc/SwaggerConfiguration.java
@@ -0,0 +1,20 @@
+package me.dio.doc;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfiguration {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+	    Server server = new Server();
+	    server.setUrl("https://sdw-2023-api-production-sellan.up.railway.app");
+	    return new OpenAPI().servers(List.of(server));
+	}
+}
