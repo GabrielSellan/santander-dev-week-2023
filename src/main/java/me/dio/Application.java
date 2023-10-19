8593e62337fcc277ed46d7586df1437ef0b0f011
GabrielSellan <gabrielsellan2002@gmail.com>
2023-10-18 21:17:53 -0300

    Bugfix CORS(2)

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,6 +1,7 @@
 package me.dio;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
